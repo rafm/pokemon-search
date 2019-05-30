681d9273a7c4f3d93ba3f3e7309f2c1abd4ca342
Rafael Maia <rafael.augusto.fm@outlook.com>
2019-05-29 22:23:24 -0300

    Adding error messages and style on the screen.

--- src/main/webapp/resources/js/PokemonSearch.js
+++ src/main/webapp/resources/js/PokemonSearch.js
@@ -1,20 +1,37 @@
-// TODO Use fetch polyfill?
-
 const pokemonSearchButton = document.querySelector(".pokemon-search-button");
 const form = document.querySelector("#pokemon-search-form");
 const pokemonInfo = document.querySelector(".pokemon-info");
 const cityInfo = document.querySelector(".city-info");
+const pokemonErrorMessage = document.querySelector(".pokemon-error-message");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+});
 pokemonSearchButton.addEventListener("click", () => {
     const cityName = form.cityName.value.trim();
     if (cityName) {
-        fetch("/api/search?cityName="+cityName).then((response) => {
-            if (response.ok) {
-                response.json().then((pokemonSearchData) => {
-                    pokemonInfo.textContent = buildPokemonInfoText(pokemonSearchData);
-                    cityInfo.textContent = buildCityInfoText(cityName, pokemonSearchData);
-                });
-            }
-        });
+        fetch("/api/search?cityName="+cityName)
+            .then((response) => {
+                if (response.ok) {
+                    response.json().then((pokemonSearchData) => {
+                        pokemonErrorMessage.textContent = "";
+                        pokemonInfo.textContent = buildPokemonInfoText(pokemonSearchData);
+                        cityInfo.textContent = buildCityInfoText(cityName, pokemonSearchData);
+                    });
+                } else if (response.status == 404) {
+                    pokemonErrorMessage.textContent = "Não conseguimos encontrar esta cidade. = (";
+                    pokemonInfo.textContent = "";
+                    cityInfo.textContent = "";
+                } else if (response.status >= 500) {
+                    pokemonErrorMessage.textContent = "Desculpe, mas o serviço está indisponível no momento. = (";
+                    pokemonInfo.textContent = "";
+                    cityInfo.textContent = "";
+                }
+            })
+            .catch(() => {
+                pokemonErrorMessage.textContent = "Ocorreu um problema ao tentar se conectar com o servidor! = O";
+                pokemonInfo.textContent = "";
+                cityInfo.textContent = "";
+            });
     }
 });
 
