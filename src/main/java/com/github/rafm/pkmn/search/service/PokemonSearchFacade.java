6d35852d91b8c80d3dc787c2c2cbe3842413b00b
Rafael Maia <rmaia@daitangroup.com>
2019-06-04 16:46:57 -0300

    Creating PokemonSearchFacade's javadoc.

--- src/main/java/com/github/rafm/pkmn/search/service/PokemonSearchFacade.java
+++ src/main/java/com/github/rafm/pkmn/search/service/PokemonSearchFacade.java
@@ -14,9 +14,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class PokemonSearchFacade {
 
-    // Defined as static to guarantee only one variable, independent of the bean's scope.
-    // But event without the static modifier, this will work properly because this Facade's scope is the default Singleton
-    // and Spring will guarantee the existence of only one instance of this class and, consequently, of the variable too.
+    /**
+     * Defined as static to guarantee only one variable, independent of the bean's scope.
+     * But event without the static modifier, this will work properly because this Facade's scope is the default Singleton
+     * and Spring will guarantee the existence of only one instance of this class and, consequently, of the variable too.
+     */
     private static String lastPokemonName = "";
     private final Random RANDOM = new Random();
 
@@ -29,6 +31,12 @@ public class PokemonSearchFacade {
     @Autowired
     private PokemonService pokemonService;
 
+    /**
+     * Method to search for a random non-repeated pokemon based on a city.
+     * 
+     * @param cityName to search for a pokemon
+     * @return the pokemon found on the passed city
+     */
     public PokemonSearchResponse searchPokemonByCityName(String cityName) {
         WeatherResponse weatherResponse = weatherApiClient.searchByCityName(cityName);
         PokemonType pokemonType = pokemonService.retrieveInhabitedPokemonTypeBasedOnWeather(weatherResponse.isRaining(), weatherResponse.getTemperature());
@@ -37,10 +45,16 @@ public class PokemonSearchFacade {
         return new PokemonSearchResponse(weatherResponse.getTemperature(), weatherResponse.isRaining(), pokemonName);
     }
 
-    // TODO Improve concurrency solution performance (because of the random logic being executed while the resource is locked)
-    // Synchronized last pokemon name to deal with concurrency when reading/writing this variable, as there will be only one
-    // accessed by multiple clients/requests at the same time. Just like a read commited isolation level in a DB transaction,
-    // as i've locked this variable before reading and writing, no client will read a dirty value of this variable.
+    /**
+     * Synchronized last pokemon name to deal with concurrency when reading/writing this variable, as there will be only one
+     * accessed by multiple clients/requests at the same time. Just like a read commited isolation level in a DB transaction,
+     * as i've locked this variable before reading and writing, no client will read a dirty value of this variable.
+     * 
+     * TODO: Improve concurrency solution performance (because of the random logic being executed while the resource is locked).
+     * 
+     * @param pokemonNames list with the pokemon names to pick a random one
+     * @return a random pokemon name from the passed pokemon names list
+     */
     private synchronized String pickRandomPokemonName(List<String> pokemonNames) {
         String pokemonName;
         do {
